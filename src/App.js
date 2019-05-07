ea0195672c37bf6fe217f554dab04442851075d1
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-05-07 18:53:47 -0400

    Traditional Redux-Connect version.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { hot } from "react-hot-loader";
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux';
 
 // TODO: Determine if babel-plugin-transform-require-ignore can work here instead of canUseDOM
 let canUseDOM = !!((typeof window !== 'undefined' && window.document && window.document.createElement));
@@ -11,21 +11,34 @@ if (canUseDOM) {
     require('./App.scss');
 }
 
-const onIncrement = () => ({ type: "counter/increment" });
-const onDecrement = () => ({ type: "counter/decrement" });
+const Counter = ({ count, onIncrement, onDecrement }) => (
+    <div className="App">
+        <p>{count}</p>
+        <button onClick={onIncrement}>increment (+)</button>
+        <button onClick={onDecrement}>decrement (-)</button>
+    </div>
+);
 
-const App = () => {
-    const count = useSelector(
-        state => state.count
-    );
-    const dispatch = useDispatch();
-    return (
-        <div className="App">
-            <p>{count}</p>
-            <button onClick={() => dispatch(onIncrement())}>increment (+)</button>
-            <button onClick={() => dispatch(onDecrement())}>decrement (-)</button>
-        </div>
-    );
+const mapStateToProps = state => {
+    return {
+        count: state.count
+    };
 };
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrement: () => {
+            dispatch({ type: 'counter/increment' });
+        },
+        onDecrement: () => {
+            dispatch({ type: 'counter/decrement' });
+        }
+    };
+};
+
+const App = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
 
 export default hot(module)(App);
