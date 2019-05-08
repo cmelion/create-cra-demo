81cf88520590919916d8ae415e172faae871db74
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-05-07 21:27:49 -0400

    Import Action creators and use bindActionCreators to improve readability.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { counter } from './dux/counter';
+
 
 // TODO: Determine if babel-plugin-transform-require-ignore can work here instead of canUseDOM
 let canUseDOM = !!((typeof window !== 'undefined' && window.document && window.document.createElement));
@@ -11,19 +14,16 @@ if (canUseDOM) {
     require('./App.scss');
 }
 
-const onIncrement = () => ({ type: "counter/increment" });
-const onDecrement = () => ({ type: "counter/decrement" });
-
 const App = () => {
     const count = useSelector(
         state => state.count
     );
-    const dispatch = useDispatch();
+    const actions = bindActionCreators(counter.actions, useDispatch());
     return (
         <div className="App">
             <p>{count}</p>
-            <button onClick={() => dispatch(onIncrement())}>increment (+)</button>
-            <button onClick={() => dispatch(onDecrement())}>decrement (-)</button>
+            <button onClick={ actions.increment }>increment (+)</button>
+            <button onClick={ actions.decrement }>decrement (-)</button>
         </div>
     );
 };
