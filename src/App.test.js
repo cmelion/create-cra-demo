fd6f62468fd363e503c4c9b1344b0de994487596
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-05-14 16:22:37 -0400

    Fix unit test.  Add Chai.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Counter from './components/counter';
+import InputForm from './components/input-form';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(InputForm)).to.have.lengthOf(1);
+    expect(wrapper.find(Counter)).to.have.lengthOf(1);
 });
