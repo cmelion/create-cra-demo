0052e781d610b05fc3d6c3fa50b8f63b89a5c797
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-05-29 13:57:14 -0400

    Bring form test coverage up to 100%.

--- src/components/input-form/.test.js
+++ src/components/input-form/.test.js
@@ -4,8 +4,7 @@ import { mount } from "enzyme";
 import wait from "waait";
 
 let ui;
-
-describe("When I insert a wrong email ", () => {
+describe("Registration form", () => {
     // TODO: Fix needed for - Warning: An update to Formik inside a test was not wrapped in act(...).
     // https://github.com/jaredpalmer/formik/issues/1543
     // for now just disable warnings in this test.
@@ -17,32 +16,131 @@ describe("When I insert a wrong email ", () => {
         console.error = originalError;
     });
 
-    beforeEach(() => {
-        ui = mount(<InputForm />);
+    describe("When I don't enter a user name ", () => {
+        beforeEach(() => {
+            ui = mount(<InputForm/>);
 
-        const emailField = ui.find("#email").find("input");
+            const nameField = ui.find("#name").find("input");
 
-        //insert a wrong email
-        emailField.simulate("change", {
-            target: {
-                name: "email",
-                value: "foo"
-            }
+            //insert a wrong email
+            nameField.simulate("change", {
+                target: {
+                    name: "name",
+                    value: ""
+                }
+            });
+
+            //simulate the blur
+            nameField.simulate("blur");
         });
 
-        //simulate the blur
-        emailField.simulate("blur");
+        it("The missing name error is displayed", async () => {
+            await wait(0);
+            ui.update();
+            const errors = ui.find('p[children="Name is required"]');
+            expect(errors.length).toBeGreaterThan(0);
+        });
+
     });
 
-    it("The error is displayed", async () => {
-        await wait(0);
-        ui.update();
-        const errors = ui.find('p[children="Enter a valid email"]');
-        expect(errors.length).toBeGreaterThan(0);
+    describe("When I enter a wrong email ", () => {
+        beforeEach(() => {
+            ui = mount(<InputForm/>);
+
+            const emailField = ui.find("#email").find("input");
+
+            //insert a wrong email
+            emailField.simulate("change", {
+                target: {
+                    name: "email",
+                    value: "foo"
+                }
+            });
+
+            //simulate the blur
+            emailField.simulate("blur");
+        });
+
+        it("The new email value should be displayed", () => {
+            const emailField = ui.find("#email").find("input");
+            expect(emailField.prop("value")).toContain("foo");
+        });
+
+        it("The email error is displayed", async () => {
+            await wait(0);
+            ui.update();
+            const errors = ui.find('p[children="Enter a valid email"]');
+            expect(errors.length).toBeGreaterThan(0);
+        });
+
     });
 
-    it("The new email value should be displayed", () => {
-        const emailField = ui.find("#email").find("input");
-        expect(emailField.prop("value")).toContain("foo");
+    describe("When I enter a password that is too short ", () => {
+        beforeEach(() => {
+            ui = mount(<InputForm/>);
+
+            const passwordField = ui.find("#password").find("input");
+
+            //insert a short password
+            passwordField.simulate("change", {
+                target: {
+                    name: "password",
+                    value: "foo"
+                }
+            });
+
+            //simulate the blur
+            passwordField.simulate("blur");
+        });
+
+        it("The new password value should be displayed", () => {
+            const passwordField = ui.find("#password").find("input");
+            expect(passwordField.prop("value")).toContain("foo");
+        });
+
+        it("The password error is displayed", async () => {
+            await wait(0);
+            ui.update();
+            const errors = ui.find('p[children="Password must contain at least 8 characters"]');
+            expect(errors.length).toBeGreaterThan(0);
+        });
+
+    });
+
+    describe("When passwords don't match ", () => {
+        beforeEach(() => {
+            ui = mount(<InputForm/>);
+
+            const passwordField = ui.find("#password").find("input");
+            const confirmPassword = ui.find("#confirmPassword").find("input");
+
+            //insert a valid password
+            passwordField.simulate("change", {
+                target: {
+                    name: "password",
+                    value: "fooBarBaz1"
+                }
+            });
+
+            //insert a mismatched confirm password
+            confirmPassword.simulate("change", {
+                target: {
+                    name: "confirmPassword",
+                    value: "foo"
+                }
+            });
+
+
+            //simulate the blur
+            confirmPassword.simulate("blur");
+        });
+
+        it("The password error is displayed", async () => {
+            await wait(0);
+            ui.update();
+            const errors = ui.find('p[children="Password does not match"]');
+            expect(errors.length).toBeGreaterThan(0);
+        });
+
     });
 });
