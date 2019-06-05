588129df64ddd0173248d9ed2fc9370ff2d7accf
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-06-05 15:07:15 -0400

    Add hooks useReducer implementation.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import './App.scss';
 
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { ...state, count: state.count + 1 };
+        case 'decrement':
+            return { ...state, count: state.count - 1 };
+        default:
+            throw new Error();
+    }
+};
+
 const useCounter = (initialValue) => {
-    const [count, setCount] = React.useState(initialValue);
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
-    return { count, increment, decrement };
+    const [state, dispatch] = React.useReducer(counterReducer, { count: initialValue });
+    const increment = () => dispatch({ type: 'increment' });
+    const decrement = () => dispatch({ type: 'decrement' });
+    return { state, increment, decrement };
 };
 
 const App = () => {
-    const { count, increment, decrement } = useCounter(0);
+    const { state: { count }, increment, decrement } = useCounter(0);
     return (
         <div className="App">
             <p>Learn React</p>
